b620a127d3c83851b0ba18ffc25908cb1b2088f1
devanshisachan0525-max <devanshisachan0525@gmail.com>
2026-08-26 19:46:02 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
     }
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.7-flash:generateContent",
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent",,
       {
         method: "POST",
         headers: {
